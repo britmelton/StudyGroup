ecd776cb10c442f6f6fc07c36bf1cf36a2745677
britmelton <brittany134340@gmail.com>
2023-09-20 17:45:57 -0400

    added value-object proj

---
+++ value-object/Source/Foo.cs
@@ -0,0 +1,11 @@
+﻿namespace Value.Object.Source;
+
+public class Foo : ValueObject
+{
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return (byte)255;
+        yield return (byte)0;
+        yield return (byte)0;
+    }
+}
